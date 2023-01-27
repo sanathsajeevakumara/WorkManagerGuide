e7bb6ed67da12bfefe9690f3159fb71eb4329369
Sanath <sanath@entrusttitle.com>
2023-01-27 12:35:13 +0530

    Project Completed

--- app/src/main/java/com/sanathcoding/workmanagerguide/MainActivity.kt
+++ app/src/main/java/com/sanathcoding/workmanagerguide/MainActivity.kt
@@ -10,14 +10,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.work.*
